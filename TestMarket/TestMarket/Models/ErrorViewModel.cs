91b444153733fdc7dc3049ccfbcb78c378f7cc63
jscalpo <51896602+jscalpo@users.noreply.github.com>
2019-08-19 10:58:25 +0800

    Initial Upload
    Initial Upload

---
+++ TestMarket/TestMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
